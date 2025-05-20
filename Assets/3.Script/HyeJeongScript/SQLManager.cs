aeefcd612432f4de237507e2c346965dee9e6349
HyejeongOk <oghyejeong2@gmail.com>
2025-05-20 15:53:41 +0900

    v0.0.28] feat : 회원정보(닉네임, 비밀번호) 변경 수정
    회원정보 변경 : 닉네임, 비밀번호 버튼
    -닉네임 버튼 클릭 : 닉네임 변경창
    - 비밀번호 버튼 클릭 : 비밀번호 변경창

--- Assets/3.Script/HyeJeongScript/SQLManager.cs
+++ Assets/3.Script/HyeJeongScript/SQLManager.cs
@@ -12,8 +12,8 @@ using LitJson;
 public class User_info
 {
     public string User_name { get; private set; }
-    public string User_Password { get; private set; }
-    public string User_Nickname { get; private set; }
+    public string User_Password { get; set; }
+    public string User_Nickname { get; set; }
 
     public User_info(string name, string password, string nickname)
     {
@@ -298,7 +298,62 @@ public class SQLManager : MonoBehaviour
 
 
     // 회원정보 수정
-    public bool Updateinfo(string id, string curname, string newname, string newpwd)
+    // 1단계 : 닉네임과 비밀번호 중 한 개를 선택한다.
+    // 2단계 : 닉네임 선택하면 닉네임만 변경, 비밀번호를 선택하면 비밀번호만 변경
+
+    // 닉네임 변경 함수
+    public bool UpdateNicknameinfo(string id, string currentname, string newname)
+    {
+        try
+        {
+            if (!connection_check(con))
+            {
+                return false;
+            }
+            //닉네임 중복 확인
+            string sqlcheckname =
+                string.Format(@"SELECT COUNT(*) FROM user_info WHERE Nickname = '{0}';", newname);
+            MySqlCommand checkcmd = new MySqlCommand(sqlcheckname, con);
+            object resultObj = checkcmd.ExecuteScalar();
+            int count = Convert.ToInt32(resultObj);
+
+            if(count > 0)
+            {
+                Debug.Log($"중복된 닉네임 :  {newname}");
+                return false;
+            }
+
+            //쿼리문
+            //UPDATE user_info SET User_Password='4696' WHERE User_Name='옥혜정';
+            string sqlupdatename =
+                string.Format(@"UPDATE user_info SET Nickname = '{0}' WHERE Name ='{1}';", newname, id);
+            MySqlCommand cmd = new MySqlCommand(sqlupdatename, con);
+
+            int result = cmd.ExecuteNonQuery(); // 성공시 n개 반환
+
+            if (result > 0)
+            {
+                info.User_Nickname = newname;
+                Debug.Log("닉네임 변경 완료");
+                return true;
+            }
+
+            else
+            {
+                Debug.Log("변경 실패.");
+                return false;
+            }
+        }
+
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+            return false;
+        }
+    }
+
+    // 비밀번호 변경
+    public bool Updatepasswordinfo(string id, string newpassword)
     {
         try
         {
@@ -309,22 +364,22 @@ public class SQLManager : MonoBehaviour
 
             //쿼리문
             //UPDATE user_info SET User_Password='4696' WHERE User_Name='옥혜정';
-            string sqlupdate =
-                string.Format(@"UPDATE user_info SET Nickname = '{0}', Password='{1}' WHERE Nickname='{2}';", newname, newpwd,  curname);
-            MySqlCommand cmd = new MySqlCommand(sqlupdate, con);
+            string sqlupdatepwd =
+                string.Format(@"UPDATE user_info SET Password = '{0}' WHERE Name ='{1}';", newpassword, id);
+            MySqlCommand cmd = new MySqlCommand(sqlupdatepwd, con);
 
             int result = cmd.ExecuteNonQuery(); // 성공시 n개 반환
 
             if (result > 0)
             {
-                info = new User_info(id, newpwd, newname);
-                Debug.Log("회원정보 수정 완료");
+                info.User_Password = newpassword;
+                Debug.Log("비밀번호 변경 완료");
                 return true;
             }
 
             else
             {
-                Debug.Log("수정할 수 없습니다.");
+                Debug.Log("변경 실패.");
                 return false;
             }
         }
