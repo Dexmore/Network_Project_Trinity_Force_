d98352f60fc64bff78501b2ed1130a86b3bae2bc
kisickang <kanggoni1231@gmail.com>
2025-05-23 20:12:07 +0900

    v 0.0.74 ] Edit : Server IP

--- Assets/3.Script/KSScript/NetWork/ConnetServer.cs
+++ Assets/3.Script/KSScript/NetWork/ConnetServer.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 public class ConnetServer : MonoBehaviour
 {
     private NetworkManager netMgr;
-    [SerializeField] private string serverIp = "3.38.169.196"; // AWS 서버 IP
+    [SerializeField] private string serverIp = "3.36.126.156"; // AWS 서버 IP
 //    private string RoomSelectScene = "RoomSelectScene"; // 씬 이름 직접 할당
 
     private void Start()
