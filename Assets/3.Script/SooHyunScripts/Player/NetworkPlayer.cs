57998d9b1df3b98b92716a712075927d9b78f113
janghyeok <janghuck0410@naver.com>
2025-05-26 21:52:03 +0900

    Revert "v0.0.126] FIX"
    This reverts commit 08ceef246e151b509ecf2f7075f937b749954c85.

--- Assets/3.Script/SooHyunScripts/Player/NetworkPlayer.cs
+++ Assets/3.Script/SooHyunScripts/Player/NetworkPlayer.cs
@@ -1,137 +1,15 @@
 ﻿using Mirror;
 using UnityEngine;
-using System.Collections;
 
 public class NetworkPlayer : NetworkBehaviour
 {
-    [Header("Game Data")]
     [SyncVar] public bool HasSubmitted = false;
     [SyncVar] public string lastText = "";
     [SyncVar] public int playerIndex = -1;
-
-    [Header("Lobby Info")]
-    [SyncVar(hook = nameof(OnReadyChanged))] public bool isReady;
-    [SyncVar(hook = nameof(OnNicknameChanged))] public string playerName;
-
-    public delegate void ChatMessageHandler(string message, string senderName);
-    public static event ChatMessageHandler OnChatMessage;
-
-    private static Coroutine startGameRoutine;
-
-    public override void OnStartLocalPlayer()
-    {
-        string nick = SQLManager.instance?.info?.User_Nickname ?? "Unknown";
-        CmdSetNickname(nick);
-    }
-
-    public override void OnStartClient()
-    {
-        base.OnStartClient();
-        StartCoroutine(WaitAndRegisterToLobby());
-    }
-
-    public override void OnStopClient()
-    {
-        if (LobbyUserManager.Instance != null && !string.IsNullOrEmpty(playerName))
-            LobbyUserManager.Instance.RemoveUser(playerName);
-
-        if (!string.IsNullOrEmpty(playerName))
-            LobbyPopupUIManager.Instance?.ShowPopup($"{playerName}님이 퇴장했습니다.");
-    }
-
-    private IEnumerator WaitAndRegisterToLobby()
-    {
-        yield return new WaitUntil(() => LobbyUserManager.Instance != null);
-        yield return new WaitUntil(() => !string.IsNullOrEmpty(playerName));
-        yield return new WaitUntil(() =>
-            LobbyUserManager.Instance.userSlotPrefab != null &&
-            LobbyUserManager.Instance.userListParent != null
-        );
-
-        LobbyUserManager.Instance.AddUser(playerName, isReady);
-        LobbyPopupUIManager.Instance?.ShowPopup($"{playerName}님이 입장했습니다.");
-    }
-
-    void OnReadyChanged(bool _, bool newVal)
-    {
-        LobbyUserManager.Instance?.UpdateNicknameReady(playerName, newVal);
-    }
-
-    void OnNicknameChanged(string _, string __) { }
+    [SyncVar] public string playerName;
 
     [Command]
-    public void CmdSetNickname(string nick)
-    {
-        playerName = nick;
-    }
-
-    [Command]
-    public void CmdToggleReady()
-    {
-        isReady = !isReady;
-
-        if (isServer)
-        {
-            CheckAllPlayersReady();
-        }
-    }
-
-    private void CheckAllPlayersReady()
-    {
-        var players = FindObjectsOfType<NetworkPlayer>();
-        int total = players.Length;
-        int ready = 0;
-
-        foreach (var p in players)
-        {
-            if (p.isReady) ready++;
-        }
-
-        if (total >= 2 && total <= 4 && ready == total)
-        {
-            if (startGameRoutine == null)
-                startGameRoutine = StartCoroutine(StartGameAfterDelay());
-        }
-        else
-        {
-            if (startGameRoutine != null)
-            {
-                StopCoroutine(startGameRoutine);
-                startGameRoutine = null;
-                Debug.Log("[NetworkPlayer] 준비 취소됨 - 씬 전환 취소됨");
-            }
-        }
-    }
-
-    private IEnumerator StartGameAfterDelay()
-    {
-        Debug.Log("[NetworkPlayer] 모든 유저가 준비 완료됨 - 3초 후 게임 시작");
-        yield return new WaitForSeconds(3f);
-
-        if (isServer)
-        {
-            NetworkManager.singleton.ServerChangeScene("GameScene");
-        }
-    }
-
-    [Command]
-    public void CmdSendMessage(string message, string senderName)
-    {
-        RpcReceiveMessage(message, senderName);
-    }
-
-    [ClientRpc]
-    void RpcReceiveMessage(string message, string senderName)
-    {
-        OnChatMessage?.Invoke(message, senderName);
-    }
-
-    // ---------- 게임 제출 관련 ----------
-    [Command]
-    public void CmdSetSubmitted(bool value)
-    {
-        HasSubmitted = value;
-    }
+    public void CmdSetSubmitted(bool value) { HasSubmitted = value; }
 
     [Command]
     public void CmdSetText(string value)
